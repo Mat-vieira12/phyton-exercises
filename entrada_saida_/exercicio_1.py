b8c3a4db330aef63f94aa9ee5c881593b20100ac
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-05-07 09:34:07 -0300

    Calc_soma_e_prod.py

---
+++ entrada_saida_/exercicio_1.py
@@ -0,0 +1,7 @@
+#Soma e produto de dois numeros
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+soma = (num1 + num2)
+produto = (num1 * num2)
+print(f'A soma dos dois número é: {soma}')
+print(f'O produto dos dois número é: {produto}')
