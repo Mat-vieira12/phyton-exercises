f2c62e0b872b908de776bbb72ec2c68ed2829b76
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-05-07 10:57:38 -0300

    area_triangulo.py

---
+++ entrada_saida_/exercicio_3.py
@@ -0,0 +1,5 @@
+#Área triângulo
+base = float(input('Digite o valor da base em cm: '))
+altura = float(input('Digite o valor da altura em cm: '))
+area= (base*altura)/2
+print(f'O valor da área desse triângulo é: {area}')
