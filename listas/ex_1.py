a19ec02fdcf85ea611634d5a996c235275f3a098
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-05-23 11:49:13 -0300

    criacao.py

---
+++ listas/ex_1.py
@@ -0,0 +1,11 @@
+numeros= [1,2,3,4,5,6,7,8,9,10]
+print(f'Lista: {numeros} ')
+dobro= []
+for numero in numeros:
+    dobro.append(numero * 2)
+print(f'Dobro: {dobro}')
+
+
+#lista = [1, 2, 3, 4, 5]
+#dobro = [x * 2 for x in lista]
+#print(dobro)
