6a04a006d0fd1974da0149b06f093ae7e5032ff3
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-05-28 10:19:51 -0300

    update.py

--- listas/ex_2.py
+++ listas/ex_2.py
@@ -3,5 +3,9 @@ iteracao=1
 while iteracao<=5:
     numero=int(input("Por favor, digite um número: "))
     iteracao+=1
-    numeros.append
+    numeros.append(numero)
 print(f'Lista: {numeros}')
+print(f'A soma dos números é: {sum(numeros)}')
+print(f'O maior número é: {max(numeros)}')
+print(f'O menor número é: {min(numeros)}')
+print(f'A média é de: {sum(numeros)/len(numeros)}')
